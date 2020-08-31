d0df08f0e80c733819765cda66c35cd532130e37
Ufuk Remzi Ünsal <50747191+ufukunsal98@users.noreply.github.com>
2020-08-31 13:45:08 +0300

    Add files via upload

---
+++ intro/src/Notfound.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Notfound extends Component {
+    render() {
+        return (
+            <div>
+                Not Founds
+            </div>
+        );
+    }
+}
+
+export default Notfound;
